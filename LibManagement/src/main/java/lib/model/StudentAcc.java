af79a5492a8e7a434133eae46dffa41ad1fb802e
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2023-03-27 08:04:40 +0200

    aa

--- LibManagement/src/main/java/lib/model/StudentAcc.java
+++ LibManagement/src/main/java/lib/model/StudentAcc.java
@@ -1,16 +1,22 @@
 package lib.model;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="STUDENTS")
-public class StudentAcc {
+public class StudentAcc  implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Integer libId;
 	Integer enrollmentNo;
 	String firstName;
@@ -63,8 +69,6 @@ public class StudentAcc {
 		return "StudentAcc [enrollmentNo=" + enrollmentNo + ", firstName=" + firstName + ", lastName=" + lastName
 				+ ", libId=" + libId + "]";
 	}
-	
-	
 	
 
 }
