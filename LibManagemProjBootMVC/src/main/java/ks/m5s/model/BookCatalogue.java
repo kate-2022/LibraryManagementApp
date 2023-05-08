b0b1bc80cb196db2d86f0b1167b3e051ce08d0fc
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2023-05-08 18:59:46 +0200

    ba

--- LibManagemProjBootMVC/src/main/java/ks/m5s/model/BookCatalogue.java
+++ LibManagemProjBootMVC/src/main/java/ks/m5s/model/BookCatalogue.java
@@ -7,31 +7,33 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import ks.m5s.service.BookStatus;
+//import ks.m5s.service.BookStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Component
 public class BookCatalogue {
 	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long bookId;
+	private Integer bookId;
 	private String bookName;
 	private String authorLastName;
 	private String authorFirstName;
 	private Date yearOfPublish;
 	
-	@Autowired
-	BookStatus status;
-	
+//	@Autowired
+//	BookStatus status;
+//	
 
 
 }
--- LibManagemProjBootMVC/src/main/java/ks/m5s/service/BookStatus.java
+++ LibManagemProjBootMVC/src/main/java/ks/m5s/service/BookStatus.java
@@ -1,5 +1,6 @@
 package ks.m5s.service;
 
+import java.sql.Date;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
@@ -7,25 +8,29 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import ks.m5s.model.BookCatalogue;
+import lombok.AllArgsConstructor;
+//import ks.m5s.model.BookCatalogue;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Component
 public class BookStatus {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long bookId;
+	private Integer bookId;
 	private Boolean bookAvailable;
 	private LocalDate dateOfLoan;
 	private Integer libIdOfStudentLoan;
 
 	
-	@Autowired 
-	BookCatalogue catalogue;
+//	@Autowired 
+//	BookCatalogue catalogue;
 }
