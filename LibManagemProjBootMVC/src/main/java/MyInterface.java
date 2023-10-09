0ac8e9f50f68c6dd5c633a9bfecb3a36829b8771
kate-2022 <101457059+kate-2022@users.noreply.github.com>
2023-10-09 21:21:23 +0200

    caa

--- LibManagemProjBootMVC/src/main/java/MyInterface.java
+++ LibManagemProjBootMVC/src/main/java/MyInterface.java
@@ -3,13 +3,6 @@ public interface MyInterface {
 	
 	String returnAnyBookEntry();
 	
-	default String returnAConfirmationThisIsPossible() {
-		return "..this obviously works!!! ";
-		
-	}
-	static String letsTryThisAsWell() {
-		return "it was the brackets.. again! :P ";
-	}
 	
 
 }
